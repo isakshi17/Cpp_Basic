2efdb88d3c088fd051d2351db0256b9dc465424b
isakshi17 <isakshish17@gmail.com>
2024-08-22 21:09:47 +0530

    new code added

--- letters.cpp
+++ letters.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main() {
   char ch;
-  cout << "Enter the character\n";
-  cin >> ch;
+  cout<<"Enter the character\n";
+  cin>>ch;
   if (ch >= 'a' && ch <= 'z') {
     cout << "The character is in lowercase\n";
   } else if (ch >= 'A' && ch <= 'Z') {
